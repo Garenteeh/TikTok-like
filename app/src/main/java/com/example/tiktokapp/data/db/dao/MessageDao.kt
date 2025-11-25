afc8ad483233e58b26e2987fd887589f3c26f6b5
= <brochettes40@outlook.fr>
2025-11-26 00:11:11 +0100

    Private messages - New DAO subject and message

--- app/src/main/java/com/example/tiktokapp/data/db/dao/MessageDao.kt
+++ app/src/main/java/com/example/tiktokapp/data/db/dao/MessageDao.kt
@@ -1,30 +1,20 @@
 package com.example.tiktokapp.data.db.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.tiktokapp.data.db.entities.MessageEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MessageDao {
-
-    @Query("SELECT * FROM message ORDER BY timestamp DESC")
-    suspend fun getAllMessages(): List<MessageEntity>
-
-    @Query("SELECT * FROM message WHERE user_id = :id")
-    suspend fun getMessageBySubject(videoId: String): MessageEntity?
+    @Query("SELECT * FROM message WHERE subjectId = :subjectId ORDER BY timestamp DESC")
+    fun getForSubject(subjectId: String): Flow<List<MessageEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMessage(video: MessageEntity)
+    suspend fun insert(message: MessageEntity)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(videos: List<MessageEntity>)
+    @Query("DELETE FROM message WHERE id = :messageId")
+    suspend fun deleteById(messageId: String)
 
-    @Query("    val user: String,DELETE FROM messages")
-    suspend fun deleteAllMessages()
-
-    @Query("DELETE FROM messages WHERE id = :id")
-    suspend fun deleteMessageById(id: String)
+    @Query("DELETE FROM message WHERE subjectId = :messageId")
+    suspend fun deleteBySubject(messageId: String)
 }
-
--- app/src/main/java/com/example/tiktokapp/data/db/dao/SubjectDao.kt
+++ app/src/main/java/com/example/tiktokapp/data/db/dao/SubjectDao.kt
@@ -4,21 +4,31 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import com.example.tiktokapp.data.db.entities.SubjectEntity
-import com.example.tiktokapp.data.db.entities.VideoEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SubjectDao {
-    @Query("SELECT * FROM subject WHERE user = :id")
-    suspend fun getSubjectsByUser(id: String): List<SubjectEntity>
+    @Query("SELECT * FROM subject WHERE user = :userId ORDER BY timestamp DESC")
+    fun getSubjectsByUser(userId: String): Flow<List<SubjectEntity>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Query("SELECT COUNT(*) FROM subject WHERE user = :userId")
+    suspend fun getSubjectsCountByUser(userId: String): Int
+
+    @Query("SELECT * FROM subject WHERE id = :subjectId")
+    suspend fun getSubjectById(subjectId: String): SubjectEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertSubject(subject: SubjectEntity)
 
-    @Query(" val subject: String,DELETE FROM subject")
-    suspend fun deleteAllSubjects()
+    @Update
+    suspend fun updateSubject(subject: SubjectEntity)
 
-    @Query("DELETE FROM subject WHERE id = :id;")
+    @Query("DELETE FROM subject WHERE id = :id")
     suspend fun deleteSubjectById(id: String)
-}
 
+    @Query("DELETE FROM subject")
+    suspend fun deleteAllSubjects()
+}
